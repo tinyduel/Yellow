7deecd4adbc05396abe8ffad37a7953b9a6d7c93
Senre <44935191+Senre@users.noreply.github.com>
2019-08-20 03:25:48 +0800

    Update rate.js

--- commands/rate.js
+++ commands/rate.js
@@ -4,6 +4,10 @@ function doRateto10(){
     let rand = [-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,9,10];
     return rand[Math.floor(Math.random()*rand.length)];
 }
+function doRateto5(){
+    let rand = [8,9,10,11,12];
+    return rand[Math.floor(Math.random()*rand.length)];
+}
 
 
 exports.run = (client, message, args) => {
@@ -11,11 +15,11 @@ exports.run = (client, message, args) => {
     let mentionedUser = message.mentions.users.first();
     let askingUser = message.author.username;
     console.log(message);
-    if(message.isMentioned(client.user)){return message.channel.send("I'd give *myself* an **11/10**  😎");}
+    if(message.isMentioned(client.user)){return message.channel.send(`I'd give ${client.user.username} a/an **${doRateto5()}/10**  😎`);}
     if((mentionedUser == null || mentionedUser.username == askingUser)){
-        message.channel.send(`I'd give you a **${judgement}/10**, **${askingUser}**`);
+        message.channel.send(`I'd give you a/an **${judgement}/10**, **${askingUser}**`);
     }else{
-        message.channel.send(`I'd rate **${mentionedUser.username}** a **${judgement}/10** to me, **${askingUser}**`);
+        message.channel.send(`I'd rate **${mentionedUser.username}** a/an **${judgement}/10** to me, **${askingUser}**`);
     }
 };
 
